311e2c608557c446eb0b3a9782dfeb010e0e26cb
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-08 20:36:56 +0530

    rendered ReactElements using variable inside JSX

--- app.jsx
+++ app.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const headings = (
+  <>
+    <h1 className="title">Heading1</h1>
+    <h2 className="title">Heading2</h2>
+    <h2 className="title">Heading2</h2>
+  </>
+);
+
 const ContainerComponent = () => {
-  return (
-    <div className="container">
-      <h1 className="title">Heading1</h1>
-      <h2 className="title">Heading2</h2>
-      <h2 className="title">Heading2</h2>
-    </div>
-  );
+  return <div className="container">{headings}</div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
