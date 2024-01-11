81c9e16510b3e1d30b7500c2eeccbc839c29906b
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-11 13:12:11 +0530

    Header and Footer is ready

--- app.jsx
+++ app.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import logo from "./logo.png";
 
 const Header = () => {
+  const searchText = "hello";
+
   return (
     <div className="navbar">
       <div className="right">
         <img src={logo} alt="logo" />
       </div>
+      <input
+        className="searchbar"
+        type="text"
+        placeholder="Search restaurants here.."
+        value={searchText}
+        onInput={(e) => {
+          console.log(e.target.value);
+        }}
+      />
       <div className="left">
         <ul>
           <li>Home</li>
@@ -19,6 +30,28 @@ const Header = () => {
     </div>
   );
 };
+const Body = () => {
+  return <div className="main-container"></div>;
+};
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  console.log(currentYear);
+  return (
+    <div className="footer">
+      ©️ Copyright {currentYear}.Kiran Mahajan. All Rights Reserved
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<AppLayout />);
