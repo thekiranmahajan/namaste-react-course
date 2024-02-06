44c17d3916f6b5e157b081351bec522d0e1fde31
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-06 22:33:16 +0530

    feat: declaration , usage and updation of State Variable in Class based components

--- src/components/ProfileClass.jsx
+++ src/components/ProfileClass.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
 class ProfileClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
       <div>
-        <h2> Profile Class Component</h2>
+        <div>
+          Profile Class Component-- Count: {this.state.count}
+          <button
+            onClick={() =>
+              this.setState({
+                count: 1,
+              })
+            }
+          >
+            Add
+          </button>
+        </div>
       </div>
     );
   }
