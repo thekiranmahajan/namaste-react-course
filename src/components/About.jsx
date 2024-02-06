c3e7320bb4f437630cb19f649a1cb69f8369cc79
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-06 22:39:30 +0530

    passing props in functional components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about">
       <h1>About Page</h1>
-      <ProfileFunc />
+      <ProfileFunc name={"Ray"} />
       <ProfileClass />
     </div>
   );
--- src/components/ProfileFunc.jsx
+++ src/components/ProfileFunc.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const ProfileFunc = () => {
+const ProfileFunc = (props) => {
   const [count, setCount] = useState(0);
   return (
     <div>
@@ -8,6 +8,7 @@ const ProfileFunc = () => {
         Profile Functional Component-- Count: {count}{" "}
         <button onClick={() => setCount(count + 1)}>Add</button>{" "}
       </div>
+      <h2>{props.name}</h2>
     </div>
   );
 };
