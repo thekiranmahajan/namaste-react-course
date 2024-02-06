4594aaee42454c3d8cc96b387c4a375154ebbbc7
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-06 22:29:08 +0530

    feat: declaration, usage and updation of State variable in functional based component

--- src/components/ProfileFunc.jsx
+++ src/components/ProfileFunc.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProfileFunc = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h2>Profile Functional Component </h2>
+      <div>
+        Profile Functional Component-- Count: {count}{" "}
+        <button onClick={() => setCount(count + 1)}>Add</button>{" "}
+      </div>
     </div>
   );
 };
